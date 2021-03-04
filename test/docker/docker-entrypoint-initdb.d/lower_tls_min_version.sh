f4ab691af2a0c87d3a1c3c3f725deb38cbc97293
Jordan Wilson <jordanw@bitquilltech.com>
2021-03-04 10:25:38 -0800

    added more comments re: lowering TLS min version

--- test/docker/docker-entrypoint-initdb.d/lower_tls_min_version.sh
+++ test/docker/docker-entrypoint-initdb.d/lower_tls_min_version.sh
@@ -1,4 +1,5 @@
+# Driver test my_basics:t_tls_opts needs a system configured to accept at least TLSv1.0
 echo "-- Lowering Minimum TLS Protocol supported, for ODBC driver tests"
 sed 's/TLSv1.2/TLSv1.0/g' /etc/ssl/openssl.cnf > /tmp/openssl.cnf
 echo "-- New OpenSSL config now in /tmp/openssl.cnf"
-# mv /tmp/openssl.cnf /etc/ssl/openssl.cnf
+
