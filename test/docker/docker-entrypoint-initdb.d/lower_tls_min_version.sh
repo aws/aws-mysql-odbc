72cf4bcc954ec2399ba4470758722828a3bfbbc5
Jordan Wilson <37088125+jordanw-bq@users.noreply.github.com>
2021-03-17 10:29:19 -0700

    Add GitHub Actions workflow to build & test PR changes (#5)

--- test/docker/docker-entrypoint-initdb.d/lower_tls_min_version.sh
+++
@@ -1,5 +0,0 @@
-# Driver test my_basics:t_tls_opts needs a system configured to accept at least TLSv1.0
-echo "-- Lowering Minimum TLS Protocol supported, for ODBC driver tests"
-sed 's/TLSv1.2/TLSv1.0/g' /etc/ssl/openssl.cnf > /tmp/openssl.cnf
-echo "-- New OpenSSL config now in /tmp/openssl.cnf"
-
