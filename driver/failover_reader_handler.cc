8ed428e1fce06db41af772151561d780186fb1ab
justing-bq <62349012+justing-bq@users.noreply.github.com>
2021-10-29 12:24:45 -0600

    Stub out failover classes

--- driver/failover_reader_handler.cc
+++ driver/failover_reader_handler.cc
@@ -6,7 +6,12 @@
 #include "failover.h"
 
 //TODO: implement
-FAILOVER_READER_HANDLER::FAILOVER_READER_HANDLER() {}
-READER_FAILOVER_RESULT* FAILOVER_READER_HANDLER::failover(std::vector<HOST_INFO*> hosts, HOST_INFO* currentHost) { return nullptr; }
+FAILOVER_READER_HANDLER::FAILOVER_READER_HANDLER(TOPOLOGY_SERVICE* topology_service) {}
+READER_FAILOVER_RESULT* FAILOVER_READER_HANDLER::failover(std::vector<HOST_INFO*>* hosts, HOST_INFO* currentHost) { return nullptr; }
 READER_FAILOVER_RESULT* FAILOVER_READER_HANDLER::getReaderConnection(std::vector<HOST_INFO*> hosts) { return nullptr; }
 FAILOVER_READER_HANDLER::~FAILOVER_READER_HANDLER() {}
+
+MYSQL* FAILOVER_READER_HANDLER::get_reader_connection(CLUSTER_TOPOLOGY_INFO& topology_info, FAILOVER_CONNECTION_HANDLER* conn_handler, const std::function <bool()> is_canceled) {
+    //TODO: implement
+    return NULL;
+}
