d5fce28da9dff3a74f7e5034125cf561c2415e18
justing-bq <62349012+justing-bq@users.noreply.github.com>
2021-11-09 10:16:51 -0700

    TOPOLOGY_SERVICE class implementation

--- driver/failover_topology.cc
+++ driver/failover_topology.cc
@@ -15,27 +15,27 @@ CLUSTER_TOPOLOGY_INFO::CLUSTER_TOPOLOGY_INFO() {
 CLUSTER_TOPOLOGY_INFO::CLUSTER_TOPOLOGY_INFO(const CLUSTER_TOPOLOGY_INFO& src_info)
     : current_reader{ src_info.current_reader }, last_updated{ src_info.last_updated }, last_used_reader{ src_info.last_used_reader }
 {
-    for (HOST_INFO* host_info_source : src_info.writers) {
-        writers.push_back(new HOST_INFO(*host_info_source)); //default copy
+    for (auto host_info_source : src_info.writers) {
+        writers.push_back(std::make_shared<HOST_INFO>(*host_info_source)); //default copy
     }
-    for (HOST_INFO* host_info_source : src_info.readers) {
-        readers.push_back(new HOST_INFO(*host_info_source)); //default copy
+    for (auto host_info_source : src_info.readers) {
+        readers.push_back(std::make_shared<HOST_INFO>(*host_info_source)); //default copy
     }
 }
 
 CLUSTER_TOPOLOGY_INFO::~CLUSTER_TOPOLOGY_INFO() {
     for (auto p : writers) {
-        delete p;
+        p.reset();
     }
     writers.clear();
 
     for (auto p : readers) {
-        delete p;
+        p.reset();
     }
     readers.clear();
 }
 
-void CLUSTER_TOPOLOGY_INFO::add_host(HOST_INFO* host_info) {
+void CLUSTER_TOPOLOGY_INFO::add_host(std::shared_ptr<HOST_INFO> host_info) {
     host_info->is_host_writer() ? writers.push_back(host_info) : readers.push_back(host_info);
     update_time();
 }
@@ -61,7 +61,7 @@ void CLUSTER_TOPOLOGY_INFO::update_time() {
     last_updated = time(0);
 }
 
-HOST_INFO* CLUSTER_TOPOLOGY_INFO::get_writer() {
+std::shared_ptr<HOST_INFO> CLUSTER_TOPOLOGY_INFO::get_writer() {
     if (writers.size() <= 0) {
         throw "No writer available";
     }
@@ -69,7 +69,7 @@ HOST_INFO* CLUSTER_TOPOLOGY_INFO::get_writer() {
     return writers[0];
 }
 
-HOST_INFO* CLUSTER_TOPOLOGY_INFO::get_next_reader() {
+std::shared_ptr<HOST_INFO> CLUSTER_TOPOLOGY_INFO::get_next_reader() {
     int num_readers = readers.size();
     if (num_readers <= 0) {
         throw "No reader available";
@@ -89,7 +89,7 @@ HOST_INFO* CLUSTER_TOPOLOGY_INFO::get_next_reader() {
     return readers[current_reader];
 }
 
-HOST_INFO* CLUSTER_TOPOLOGY_INFO::get_reader(int i) {
+std::shared_ptr<HOST_INFO> CLUSTER_TOPOLOGY_INFO::get_reader(int i) {
     if (i < 0 || i >= readers.size()) {
         throw "No reader available at index " + i;
     }
@@ -97,8 +97,30 @@ HOST_INFO* CLUSTER_TOPOLOGY_INFO::get_reader(int i) {
     return readers[i];
 }
 
+std::shared_ptr<HOST_INFO> CLUSTER_TOPOLOGY_INFO::get_last_used_reader() {
+    return last_used_reader;
+}
+
+void CLUSTER_TOPOLOGY_INFO::set_last_used_reader(std::shared_ptr<HOST_INFO> reader) {
+    last_used_reader = reader;
+}
+
+void CLUSTER_TOPOLOGY_INFO::mark_host_down(std::shared_ptr<HOST_INFO> down_host) {
+    down_host->set_host_state(DOWN);
+    down_hosts.insert(down_host->get_host_port_pair());
+}
+
+void CLUSTER_TOPOLOGY_INFO::unmark_host_down(std::shared_ptr<HOST_INFO> host) {
+    host->set_host_state(UP);
+    down_hosts.erase(host->get_host_port_pair());
+}
+
+std::set<std::string> CLUSTER_TOPOLOGY_INFO::get_down_hosts() {
+    return down_hosts;
+}
+
 TOPOLOGY_SERVICE::TOPOLOGY_SERVICE()
-    : cluster_instance_host{ NULL }, refresh_rate_in_milliseconds{ DEFAULT_REFRESH_RATE_IN_MILLISECONDS }
+    : cluster_instance_host{ nullptr }, refresh_rate_in_milliseconds{ DEFAULT_REFRESH_RATE_IN_MILLISECONDS }
 {
     //TODO get better initial cluster id
     time_t now = time(0);
@@ -109,13 +131,13 @@ void TOPOLOGY_SERVICE::set_cluster_id(const char* cluster_id) {
     this->cluster_id = cluster_id;
 }
 
-void TOPOLOGY_SERVICE::set_cluster_instance_template(HOST_INFO* host_template) {
+void TOPOLOGY_SERVICE::set_cluster_instance_template(std::shared_ptr<HOST_INFO> host_template) {
 
     // NOTE, this may not have to be a pointer. Copy the information passed to this function.
     // Alernatively the create host function should be part of the Topologyservice, even protected or private one
     // and host information passed as separate parameters.
-    if (cluster_instance_host != NULL)
-        delete cluster_instance_host;
+    if (cluster_instance_host)
+        cluster_instance_host.reset();
 
     cluster_instance_host = host_template;
 
@@ -125,45 +147,93 @@ void TOPOLOGY_SERVICE::set_refresh_rate(int refresh_rate) {
     refresh_rate_in_milliseconds = refresh_rate; 
 }
 
-//TODO (re)consider signagures of the following functions and implement  
-HOST_INFO* TOPOLOGY_SERVICE::get_last_used_reader() {
-    // TODO
-    return nullptr; 
-}
-void TOPOLOGY_SERVICE::set_last_used_reader(HOST_INFO* reader) {
-    // TODO
+std::shared_ptr<HOST_INFO> TOPOLOGY_SERVICE::get_last_used_reader() {
+    auto topology_info = get_from_cache();
+    if (!topology_info || refresh_needed(topology_info->time_last_updated())) {
+        return nullptr;
+    }
+
+    return topology_info->get_last_used_reader();
 }
 
-std::vector<std::string>* TOPOLOGY_SERVICE::get_down_hosts() { 
-    // TODO
-    return nullptr; 
+void TOPOLOGY_SERVICE::set_last_used_reader(std::shared_ptr<HOST_INFO> reader) {
+    if (reader) {
+        std::unique_lock<std::mutex> lock(topology_cache_mutex);
+        auto topology_info = get_from_cache();
+        if (topology_info) {
+            topology_info->set_last_used_reader(reader);
+        }
+        lock.unlock();
+    }
 }
 
-void TOPOLOGY_SERVICE::mark_host_down(HOST_INFO* down_host) {
-    // TODO
+std::set<std::string> TOPOLOGY_SERVICE::get_down_hosts() {
+    std::set<std::string> down_hosts;
+    
+    std::unique_lock<std::mutex> lock(topology_cache_mutex);
+    auto topology_info = get_from_cache();
+    if (topology_info) {
+        down_hosts = topology_info->get_down_hosts();
+    }
+    lock.unlock();
+
+    return down_hosts;
 }
-void TOPOLOGY_SERVICE::unmark_host_down(HOST_INFO* host) {}
+
+void TOPOLOGY_SERVICE::mark_host_down(std::shared_ptr<HOST_INFO> down_host) {
+    if (!down_host) {
+        return;
+    }
+    
+    std::unique_lock<std::mutex> lock(topology_cache_mutex);
+    
+    auto topology_info = get_from_cache();
+    if (topology_info) {
+        topology_info->mark_host_down(down_host);
+    }
+    
+    lock.unlock();
+}
+
+void TOPOLOGY_SERVICE::unmark_host_down(std::shared_ptr<HOST_INFO> host) {
+    if (!host) {
+        return;
+    }
+    
+    std::unique_lock<std::mutex> lock(topology_cache_mutex);
+    
+    auto topology_info = get_from_cache();
+    if (topology_info) {
+        topology_info->unmark_host_down(host);
+    }
+    
+    lock.unlock();
+}
 
 void TOPOLOGY_SERVICE::clear_all() {
-    // TODO
+    std::unique_lock<std::mutex> lock(topology_cache_mutex);
+    topology_cache.clear();
+    lock.unlock();
 }
 
 void TOPOLOGY_SERVICE::clear() {
-    // TODO
+    std::unique_lock<std::mutex> lock(topology_cache_mutex);
+    topology_cache.erase(cluster_id);
+    lock.unlock();
 }
 
 TOPOLOGY_SERVICE::~TOPOLOGY_SERVICE() {
-    if (cluster_instance_host != NULL)
-        delete cluster_instance_host;
+    if (cluster_instance_host)
+        cluster_instance_host.reset();
 
     for (auto p : topology_cache) {
-        delete p.second;
+        p.second.reset();
     }
     topology_cache.clear();
 }
 
 std::unique_ptr<CLUSTER_TOPOLOGY_INFO> TOPOLOGY_SERVICE::get_cached_topology() {
-    CLUSTER_TOPOLOGY_INFO* topology_info = get_from_cache();
+    auto topology_info = get_from_cache();
     if (topology_info) {
         return std::make_unique<CLUSTER_TOPOLOGY_INFO>(*topology_info);
     }
@@ -179,8 +249,8 @@ std::unique_ptr<CLUSTER_TOPOLOGY_INFO> TOPOLOGY_SERVICE::get_topology(MYSQL* con
 {
     //TODO reconsider using this cache. It appears that we only store information for the current cluster Id.
     // therefore instead of a map we can just keep CLUSTER_TOPOLOGY_INFO* topology_info member variable.
-    CLUSTER_TOPOLOGY_INFO* topology_info = get_from_cache();
-    if (topology_info == NULL
+    auto topology_info = get_from_cache();
+    if (!topology_info
         || force_update
         || refresh_needed(topology_info->time_last_updated()))
     {
@@ -196,27 +266,29 @@ std::unique_ptr<CLUSTER_TOPOLOGY_INFO> TOPOLOGY_SERVICE::get_topology(MYSQL* con
 }
 
 // TODO consider thread safety and usage of pointers
-CLUSTER_TOPOLOGY_INFO* TOPOLOGY_SERVICE::get_from_cache() {
+std::shared_ptr<CLUSTER_TOPOLOGY_INFO> TOPOLOGY_SERVICE::get_from_cache() {
     if (topology_cache.empty())
-        return NULL;
+        return nullptr;
     auto result = topology_cache.find(cluster_id);
-    return result != topology_cache.end() ? result->second : NULL;
+    return result != topology_cache.end() ? result->second : nullptr;
 }
 
 // TODO consider thread safety and usage of pointers
-void TOPOLOGY_SERVICE::put_to_cache(CLUSTER_TOPOLOGY_INFO* topology_info) {
-    if (topology_info == NULL)
+void TOPOLOGY_SERVICE::put_to_cache(std::shared_ptr<CLUSTER_TOPOLOGY_INFO> topology_info) {
+    if (!topology_info)
         return;
     if (!topology_cache.empty())
     {
         auto result = topology_cache.find(cluster_id);
         if (result != topology_cache.end()) {
-            delete result->second;
+            result->second.reset();
             result->second = topology_info;
             return;
         }
     }
+    std::unique_lock<std::mutex> lock(topology_cache_mutex);
     topology_cache[cluster_id] = topology_info;
+    lock.unlock();
 }
 
 // TODO harmonize time function accross objects so the times are comparable
@@ -225,7 +297,7 @@ bool TOPOLOGY_SERVICE::refresh_needed(std::time_t last_updated) {
     return  time(0) - last_updated > (refresh_rate_in_milliseconds / 1000);
 }
 
-HOST_INFO* TOPOLOGY_SERVICE::create_host(MYSQL_ROW& row) {
+std::shared_ptr<HOST_INFO> TOPOLOGY_SERVICE::create_host(MYSQL_ROW& row) {
 
     //TEMP and TODO figure out how to fetch values from row by name, not by ordinal for now this enum is matching 
     // order of columns in the query
@@ -237,17 +309,18 @@ HOST_INFO* TOPOLOGY_SERVICE::create_host(MYSQL_ROW& row) {
     };
 
     if (row[SERVER_ID] == NULL) {
-        return NULL;  // will not be able to generate host endpoint so no point. TODO: log this condition?
+        return nullptr;  // will not be able to generate host endpoint so no point. TODO: log this condition?
     }
 
     std::string host_endpoint = get_host_endpoint(row[SERVER_ID]);
 
     //TODO check cluster_instance_host for NULL, or decide what is needed out of it
-    HOST_INFO* host_info = new HOST_INFO(
-        "", // TODO not putting the URL yet, maybe not necessary.
-        host_endpoint,
-        cluster_instance_host->get_port()
-    );
+    std::shared_ptr<HOST_INFO> host_info = std::make_shared<HOST_INFO>(
+        HOST_INFO(
+            "", // TODO not putting the URL yet, maybe not necessary.
+            host_endpoint,
+            cluster_instance_host->get_port()
+        ));
 
     //TODO do case-insensitive comparison
     // side note: how stable this is on the server side? If it changes there we will not detect a writter.
@@ -265,25 +338,24 @@ HOST_INFO* TOPOLOGY_SERVICE::create_host(MYSQL_ROW& row) {
 }
 
 // If no host information retrieved return NULL
-CLUSTER_TOPOLOGY_INFO* TOPOLOGY_SERVICE::query_for_topology(MYSQL* conn) {
+std::shared_ptr<CLUSTER_TOPOLOGY_INFO> TOPOLOGY_SERVICE::query_for_topology(MYSQL* conn) {
 
-    CLUSTER_TOPOLOGY_INFO* topology_info = NULL;
+    std::shared_ptr<CLUSTER_TOPOLOGY_INFO> topology_info = nullptr;
 
     MYSQL_RES* query_result = NULL;
     if ((conn != NULL) && (mysql_query(conn, RETRIEVE_TOPOLOGY_SQL) == 0) && (query_result = mysql_store_result(conn)) != NULL) {
-        topology_info = new CLUSTER_TOPOLOGY_INFO();
+        topology_info = std::make_shared<CLUSTER_TOPOLOGY_INFO>(CLUSTER_TOPOLOGY_INFO());
         MYSQL_ROW row;
         while ((row = mysql_fetch_row(query_result))) {
-            HOST_INFO* hi = create_host(row);
-            if (hi != NULL) {
-                topology_info->add_host(hi);
+            std::shared_ptr<HOST_INFO> host_info = create_host(row);
+            if (!host_info) {
+                topology_info->add_host(host_info);
             }
         }
         mysql_free_result(query_result);
 
         if (topology_info->total_hosts() == 0) {
-            delete topology_info;
-            topology_info = NULL;
+            topology_info.reset();
         }
     }
     return topology_info;
@@ -318,14 +390,14 @@ HOST_INFO::HOST_INFO()
 }
 
 HOST_INFO::HOST_INFO(std::string url, std::string host, int port)
-    : original_url{ url }, host { host }, port{ port }, is_down{ false }, is_writer{ false }
+    : original_url{ url }, host{ host }, port{ port }, host_state{ UP }, is_writer{ false }
 {
 
 }
 
 // would need some checks for nulls
 HOST_INFO::HOST_INFO(const char* url, const char* host, int port)
-    : original_url{ url }, host{ host }, port{ port }, is_down{ false }, is_writer{ false }
+    : original_url{ url }, host{ host }, port{ port }, host_state{ UP }, is_writer{ false }
 {
 
 }
@@ -362,12 +434,12 @@ bool  HOST_INFO::equal_host_port_pair(HOST_INFO& hi) {
     return get_host_port_pair() == hi.get_host_port_pair();
 }
 
-bool HOST_INFO::is_host_down() {
-    return is_down;
+HOST_STATE HOST_INFO::get_host_state() {
+    return host_state;
 }
 
-void HOST_INFO::mark_as_down(bool down) {
-    is_down = down;
+void HOST_INFO::set_host_state(HOST_STATE state) {
+    host_state = state;
 }
 
 bool HOST_INFO::is_host_writer() {
