c9f64a8f3886d35f8dee650b7c69cf7a71bcb795
Yan Wang <68562925+yanw-bq@users.noreply.github.com>
2021-11-25 11:46:09 -0800

    RDS-524 WAIT_NEW_WRITER_HANDLER (#17)
    * return writer failover result for wait new writer handler
    
    * implement wait new writer handler
    
    * close reader connection if not needed
    
    * change is_same_host_info to static method under HOST_INFO
    
    * format change
    
    * format change
    
    * change else { if to else if {
    
    * remove extra }

--- driver/failover_reader_handler.cc
+++ driver/failover_reader_handler.cc
@@ -7,8 +7,8 @@
 
 //TODO: implement
 FAILOVER_READER_HANDLER::FAILOVER_READER_HANDLER(TOPOLOGY_SERVICE* topology_service) {}
-READER_FAILOVER_RESULT* FAILOVER_READER_HANDLER::failover(std::vector<HOST_INFO*>* hosts, HOST_INFO* currentHost) { return nullptr; }
-READER_FAILOVER_RESULT* FAILOVER_READER_HANDLER::getReaderConnection(std::vector<HOST_INFO*> hosts) { return nullptr; }
+READER_FAILOVER_RESULT FAILOVER_READER_HANDLER::failover(std::vector<HOST_INFO*>* hosts, HOST_INFO* currentHost) { return READER_FAILOVER_RESULT{}; }
+READER_FAILOVER_RESULT FAILOVER_READER_HANDLER::getReaderConnection(std::shared_ptr<CLUSTER_TOPOLOGY_INFO> current_topology) { return READER_FAILOVER_RESULT{}; }
 FAILOVER_READER_HANDLER::~FAILOVER_READER_HANDLER() {}
 
 MYSQL* FAILOVER_READER_HANDLER::get_reader_connection(CLUSTER_TOPOLOGY_INFO& topology_info, FAILOVER_CONNECTION_HANDLER* conn_handler, const std::function <bool()> is_canceled) {
