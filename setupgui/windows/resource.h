f386a9ef29b8e75843ed6bd686c656aca06a7a02
Bruno Paiva Lima da Silva <brunos@bitquilltech.com>
2022-01-21 07:13:10 -0800

    RDS-345: Adding 'Allow reader connections' boolean parameter to connection string.

--- setupgui/windows/resource.h
+++ setupgui/windows/resource.h
@@ -159,6 +159,7 @@
 #define IDC_EDIT_failover_reader_connect_timeout 10082
 #define IDC_EDIT_connect_timeout		10083
 #define IDC_EDIT_network_timeout        10084
+#define IDC_CHECK_allow_reader_connections 10085
 #define IDC_BUTTON_TEST                 11014
 #define IDC_BUTTON_HELP                 11015
 #define IDC_STATIC                      -1
