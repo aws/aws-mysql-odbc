2fb8757257d33fd6885db094bafe417dde6d5174
Karen Chen <64801825+karenc-bq@users.noreply.github.com>
2022-03-08 15:29:29 -0800

    test: implement network failover integration tests and fix driver bugs (#74)
    test: implement network failover integration tests
    fix: default failover timeout set to 0 causing failover to wait for a response forever

--- integration/toxiproxy/bandwidth.h
+++ integration/toxiproxy/bandwidth.h
@@ -41,14 +41,16 @@ class BANDWIDTH : public TOXIC
 {
 public:
   BANDWIDTH(TOXIPROXY_HTTP_CLIENT* client, const std::string& toxic_list_path, const std::string& name, TOXIC_DIRECTION stream, long rate)
-    : TOXIC(client, toxic_list_path, name, stream), rate(rate)
+    : TOXIC{ client, toxic_list_path, name, stream }, rate{ rate }
   {
-    std::cout << "create_toxic: " << toxic_list_path << std::endl;
     create_toxic(toxic_list_path);
   }
 
   BANDWIDTH(TOXIPROXY_HTTP_CLIENT* client, std::string path, nlohmann::json json_object)
-    : TOXIC(client, std::move(path), std::move(json_object)) {}
+    : TOXIC{ client, path, json_object }
+  {
+    set_from_json(json_object);
+  }
 
   void set_attributes(nlohmann::json attributes) override;
   nlohmann::json get_attributes() override;
@@ -57,7 +59,7 @@ public:
   BANDWIDTH* set_rate(long rate);
 
 private:
-  long rate = 0.0;
+  long rate = 0;
 };
 
 #endif
