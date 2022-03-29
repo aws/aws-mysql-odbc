4d29441a293f9d3ba108bf63aa3ecf39ebcc3d41
Yan Wang <68562925+yanw-bq@users.noreply.github.com>
2022-03-29 11:30:25 -0700

    RDS-319 Fix memory leak from mysql lib (#109)
    * fix memory leaks on driver loading
    
    * remove debug header
    
    * spacing

--- driver/utility.cc
+++ driver/utility.cc
@@ -111,6 +111,7 @@ SQLRETURN odbc_stmt(DBC *dbc, const char *query,
   {
       const unsigned int mysql_error_code = dbc->mysql->error_code();
 
+      MYLOG_DBC_TRACE(dbc, dbc->mysql->error());
       result = set_conn_error(dbc, MYERR_S1000, dbc->mysql->error(), mysql_error_code);
 
       if (!server_alive || is_connection_lost(mysql_error_code))
@@ -118,6 +119,7 @@ SQLRETURN odbc_stmt(DBC *dbc, const char *query,
           bool rollback = (!autocommit_on(dbc) && trans_supported(dbc)) || dbc->transaction_open;
           if (rollback)
           {
+              MYLOG_DBC_TRACE(dbc, "Rolling back");
               dbc->mysql->real_query("ROLLBACK", 8);
           }
           dbc->transaction_open = false;
