dc49cde9205b7ac58c4f75f66f0020cec0ef3a9d
Yan Wang <68562925+yanw-bq@users.noreply.github.com>
2022-06-14 17:40:59 -0700

    Stub of MYSQLLIB_PROXY (#12)
    * Encapsulate all used MySQL library calls into a proxy class
    
    * fix include case
    
    * rename to mysql_proxy
    
    * replace connection with mysql_proxy
    
    * fix cmakelist
    
    * really fix cmakelist
    
    * fix tests
    
    * fix unit tests and memory leaks
    
    * fix unit tests for UNIX
    
    * fix NPE when reconnect
    
    * fix update connection during failover
    
    * not exposing MYSQL
    
    * revise comments
    
    * format
    
    * format

--- driver/topology_service.h
+++ driver/topology_service.h
@@ -32,7 +32,7 @@
 
 #include "cluster_aware_metrics_container.h"
 #include "cluster_topology_info.h"
-#include "connection.h"
+#include "mysql_proxy.h"
 #include "mylog.h"
 
 #include <map>
@@ -126,6 +126,8 @@ protected:
 
     std::shared_ptr<CLUSTER_TOPOLOGY_INFO> get_from_cache();
     void put_to_cache(std::shared_ptr<CLUSTER_TOPOLOGY_INFO> topology_info);
+
+    MYSQL_RES* try_execute_query(CONNECTION_INTERFACE* mysql_proxy, const char* query);
 };
 
 #endif /* __TOPOLOGYSERVICE_H__ */
