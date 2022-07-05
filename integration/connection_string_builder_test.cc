d6db47d74e73e234e4c8f3cb7113950ed3f486cd
Bruno Paiva Lima da Silva <brunos@bitquilltech.com>
2022-07-05 10:26:32 -0700

    Adding EFM performance tests.

--- integration/connection_string_builder_test.cc
+++ integration/connection_string_builder_test.cc
@@ -85,9 +85,10 @@ TEST_F(ConnectionStringBuilderTest, test_complete_string) {
                                    .withFailureDetectionInterval(100)
                                    .withFailureDetectionCount(4)
                                    .withMonitorDisposalTime(300)
+                                   .withEnableClusterFailover(true)
                                    .build();
 
-  const std::string expected = "DSN=testDSN;SERVER=testServer;PORT=3306;UID=testUser;PWD=testPwd;DATABASE=testDb;LOG_QUERY=0;ALLOW_READER_CONNECTIONS=1;MULTI_STATEMENTS=0;FAILOVER_T=120000;CONNECT_TIMEOUT=20;NETWORK_TIMEOUT=20;HOST_PATTERN=?.testDomain;ENABLE_FAILURE_DETECTION=1;FAILURE_DETECTION_TIME=10000;FAILURE_DETECTION_INTERVAL=100;FAILURE_DETECTION_COUNT=4;MONITOR_DISPOSAL_TIME=300;";
+  const std::string expected = "DSN=testDSN;SERVER=testServer;PORT=3306;UID=testUser;PWD=testPwd;DATABASE=testDb;LOG_QUERY=0;ALLOW_READER_CONNECTIONS=1;MULTI_STATEMENTS=0;ENABLE_CLUSTER_FAILOVER=1;FAILOVER_T=120000;CONNECT_TIMEOUT=20;NETWORK_TIMEOUT=20;HOST_PATTERN=?.testDomain;ENABLE_FAILURE_DETECTION=1;FAILURE_DETECTION_TIME=10000;FAILURE_DETECTION_INTERVAL=100;FAILURE_DETECTION_COUNT=4;MONITOR_DISPOSAL_TIME=300;";
   EXPECT_EQ(0, expected.compare(connection_string));
 }
 
@@ -129,10 +130,11 @@ TEST_F(ConnectionStringBuilderTest, test_setting_boolean_fields) {
                                    .withLogQuery(false)
                                    .withAllowReaderConnections(true)
                                    .withMultiStatements(true)
+                                   .withEnableClusterFailover(false)
                                    .withEnableFailureDetection(true)
                                    .build();
 
-  const std::string expected("DSN=testDSN;SERVER=testServer;PORT=3306;UID=testUser;PWD=testPwd;LOG_QUERY=0;ALLOW_READER_CONNECTIONS=1;MULTI_STATEMENTS=1;ENABLE_FAILURE_DETECTION=1;");
+  const std::string expected("DSN=testDSN;SERVER=testServer;PORT=3306;UID=testUser;PWD=testPwd;LOG_QUERY=0;ALLOW_READER_CONNECTIONS=1;MULTI_STATEMENTS=1;ENABLE_CLUSTER_FAILOVER=0;ENABLE_FAILURE_DETECTION=1;");
   EXPECT_EQ(0, expected.compare(connection_string));
 }
 
