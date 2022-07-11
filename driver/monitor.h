8ca87fe52e3b4790fd2d0cb6c420e8b0b5cd867b
Yan Wang <yanw@bitquilltech.com>
2022-07-11 15:42:27 -0700

    fix NPE in Monitor class, fix get_host() returning temp char*

--- driver/monitor.h
+++ driver/monitor.h
@@ -43,8 +43,8 @@ class MYSQL_MONITOR_PROXY;
 
 static const std::chrono::milliseconds thread_sleep_when_inactive = std::chrono::milliseconds(100);
 
-class MONITOR {
-public:
+class MONITOR : public std::enable_shared_from_this<MONITOR> {
+ public:
     MONITOR(
         std::shared_ptr<HOST_INFO> host_info,
         std::chrono::milliseconds monitor_disposal_time,
