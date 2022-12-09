6334ac1e20cffb8a2d9c34639741f8adce729420
Yan Wang <yanw@bitquilltech.com>
2022-12-08 16:53:04 -0800

    log ds

--- driver/connect.cc
+++ driver/connect.cc
@@ -366,6 +366,17 @@ SQLRETURN DBC::connect(DataSource *dsrc, bool failover_enabled)
       write_timeout = get_network_timeout(dsrc->write_timeout);
   }
 
+  MYLOG_DBC_TRACE(this, failover_enabled ? "Failover Enabled" : "Failover Disabled");
+  MYLOG_DBC_TRACE(this, dsrc->enable_failure_detection ? "EFM Enabled" : "EFM Disabled");
+
+  MYLOG_DBC_TRACE(this, "failure_detection_time : %u", dsrc->failure_detection_time);
+  MYLOG_DBC_TRACE(this, "failure_detection_count : %u", dsrc->failure_detection_count);
+  MYLOG_DBC_TRACE(this, "failure_detection_interval : %u", dsrc->failure_detection_interval);
+
+  MYLOG_DBC_TRACE(this, "connect_timeout : %u", connect_timeout);
+  MYLOG_DBC_TRACE(this, "read_timeout : %u", read_timeout);
+  MYLOG_DBC_TRACE(this, "write_timeout : %u", write_timeout);
+
   mysql_proxy->options(MYSQL_OPT_CONNECT_TIMEOUT, &connect_timeout);
   mysql_proxy->options(MYSQL_OPT_READ_TIMEOUT, &read_timeout);
   mysql_proxy->options(MYSQL_OPT_WRITE_TIMEOUT, &write_timeout);
