f15a0d013e17567891dba2bd3ea12f64ae134a98
Yan Wang <yanw@bitquilltech.com>
2023-03-22 10:16:58 -0700

    add cache for secrets manager proxy

--- driver/secrets_manager_proxy.cc
+++ driver/secrets_manager_proxy.cc
@@ -29,6 +29,7 @@
 
 #include <aws/secretsmanager/SecretsManagerServiceClientModel.h>
 #include <aws/secretsmanager/model/GetSecretValueRequest.h>
+#include <aws/core/auth/AWSCredentialsProviderChain.h>
 
 #include "secrets_manager_proxy.h"
 
@@ -39,9 +40,8 @@
 using namespace Aws::SecretsManager;
 
 namespace {
-    const char* DEFAULT_REGION = "us-east-1";
     const Aws::String USERNAME_KEY{ "username" };
-    const Aws::String PASSWORD_KEY{"password"};
+    const Aws::String PASSWORD_KEY{ "password" };
 }
 
 SECRETS_MANAGER_PROXY::SECRETS_MANAGER_PROXY(DBC* dbc, DataSource* ds) : SECRETS_MANAGER_PROXY(dbc, ds, nullptr) {}
@@ -51,43 +51,90 @@ SECRETS_MANAGER_PROXY::SECRETS_MANAGER_PROXY(DBC* dbc, DataSource* ds, CONNECTIO
     dbc, ds) {
 
     SecretsManagerClientConfiguration config;
-    const char* region = ds_get_utf8attr(ds->auth_region, &ds->auth_region8);
-    config.region = region ? region : DEFAULT_REGION;
-
+    Aws::Auth::DefaultAWSCredentialsProviderChain chain;
+    auto creds = chain.GetAWSCredentials();
+    auto accesskey = creds.GetAWSAccessKeyId();
+    const Aws::String region = ds_get_utf8attr(ds->auth_region, &ds->auth_region8);
+    config.region = region.empty() ? Aws::Region::US_EAST_1 : region;
     this->sm_client = SecretsManagerClient(config);
+
+    const Aws::String secret_ID = ds_get_utf8attr(ds->auth_secret_id, &ds->auth_secret_id8);
+    this->secret_key = std::make_pair(secret_ID, region);
+
     this->next_proxy = next_proxy;
 }
 
 bool SECRETS_MANAGER_PROXY::real_connect(const char* host, const char* user, const char* passwd, const char* db,
                                          unsigned int port, const char* unix_socket, unsigned long clientflag) {
 
-    const auto json_value = parse_json_value(fetch_latest_credentials());
-    const auto json_view = json_value.View();
-    const auto username = get_from_json_view(json_view, USERNAME_KEY);
-    const auto password = get_from_json_view(json_view, PASSWORD_KEY);
+    auto fetched = update_secret(false);
+    auto username = get_from_secret_json_value(USERNAME_KEY);
+    auto password = get_from_secret_json_value(PASSWORD_KEY);
+    fetched = false;
+    password = "wrong";
+    auto ret = next_proxy->real_connect(host, username.c_str(), password.c_str(), db, port, unix_socket, clientflag);
 
-    return next_proxy->real_connect(host, username.c_str(), password.c_str(), db, port, unix_socket, clientflag);
+    if (!ret && next_proxy->error_code() == ER_ACCESS_DENIED_ERROR && !fetched) {
+        // Login unsuccessful with cached credentials
+        // Try to re-fetch credentials and try again
+        fetched = update_secret(true);
+        if (fetched) {
+            username = get_from_secret_json_value(USERNAME_KEY);
+            password = get_from_secret_json_value(PASSWORD_KEY);
+            ret = next_proxy->real_connect(host, username.c_str(), password.c_str(), db, port, unix_socket, clientflag);
+        }
+    }
+
+    return ret;
 }
 
 bool SECRETS_MANAGER_PROXY::real_connect_dns_srv(const char* dns_srv_name, const char* user, const char* passwd,
                                                  const char* db, unsigned long client_flag) {
 
-    const auto json_value = parse_json_value(fetch_latest_credentials());
-    const auto json_view = json_value.View();
-    const auto username = get_from_json_view(json_view, USERNAME_KEY);
-    const auto password = get_from_json_view(json_view, PASSWORD_KEY);
+    auto fetched = update_secret(false);
+    auto username = get_from_secret_json_value(USERNAME_KEY);
+    auto password = get_from_secret_json_value(PASSWORD_KEY);
+    fetched = false;
+    password = "wrong";
+    auto ret = next_proxy->real_connect_dns_srv(dns_srv_name, username.c_str(), password.c_str(), db, client_flag);
 
-    return next_proxy->real_connect_dns_srv(dns_srv_name, username.c_str(), password.c_str(), db, client_flag);
+    if (!ret && next_proxy->error_code() == ER_ACCESS_DENIED_ERROR && !fetched) {
+        // Login unsuccessful with cached credentials
+        // Try to re-fetch credentials and try again
+        fetched = update_secret(true);
+        if (fetched) {
+            username = get_from_secret_json_value(USERNAME_KEY);
+            password = get_from_secret_json_value(PASSWORD_KEY);
+            ret = next_proxy->real_connect_dns_srv(dns_srv_name, username.c_str(), password.c_str(), db, client_flag);
+        }
+    }
+
+    return ret;
 }
 
-Aws::String SECRETS_MANAGER_PROXY::fetch_latest_credentials() const {
-    const Aws::String secret_ID = ds_get_utf8attr(ds->auth_secret_id, &ds->auth_secret_id8);
+bool SECRETS_MANAGER_PROXY::update_secret(bool force_re_fetch) {
+    bool fetched = false;
+    {
+        std::unique_lock lock(secrets_cache_mutex);
 
-    Model::GetSecretValueRequest request;
-    request.SetSecretId(secret_ID);
+        if (const auto search = secrets_cache.find(this->secret_key); search != secrets_cache.end() && !force_re_fetch) {
+            this->secret_json_value = search->second;
+        }
+        else {
+            this->secret_json_value = fetch_latest_credentials();
+            fetched = true;
+            secrets_cache[this->secret_key] = this->secret_json_value;
+        }
+    }
 
+    return fetched;
+}
+
+Aws::Utils::Json::JsonValue SECRETS_MANAGER_PROXY::fetch_latest_credentials() const {
     Aws::String secret_string;
 
+    Model::GetSecretValueRequest request;
+    request.SetSecretId(this->secret_key.first);
     auto get_secret_value_outcome = this->sm_client.GetSecretValue(request);
 
     if (get_secret_value_outcome.IsSuccess()) {
@@ -96,7 +143,7 @@ Aws::String SECRETS_MANAGER_PROXY::fetch_latest_credentials() const {
     else {
         MYLOG_DBC_TRACE(dbc, get_secret_value_outcome.GetError().GetMessage().c_str());
     }
-    return secret_string;
+    return parse_json_value(secret_string);
 }
 
 Aws::Utils::Json::JsonValue SECRETS_MANAGER_PROXY::parse_json_value(Aws::String json_string) const {
@@ -108,8 +155,10 @@ Aws::Utils::Json::JsonValue SECRETS_MANAGER_PROXY::parse_json_value(Aws::String 
     return res_json;
 }
 
-std::string SECRETS_MANAGER_PROXY::get_from_json_view(Aws::Utils::Json::JsonView view, std::string key) const {
+std::string SECRETS_MANAGER_PROXY::get_from_secret_json_value(std::string key) const {
     std::string value;
+    const auto view = this->secret_json_value.View();
+
     if (view.ValueExists(key)) {
         value = view.GetString(key);
     }
