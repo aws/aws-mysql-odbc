dac5599fc011ec19fc1b05f815da691c8e25fd50
justing-bq <justing@bitquilltech.com>
2023-05-02 10:29:02 -0700

    Disable other tests

--- integration/failover_performance_test.cc
+++ integration/failover_performance_test.cc
@@ -417,33 +417,33 @@ INSTANTIATE_TEST_CASE_P(
   FailoverPerformanceTest,
   // Test Type, Sleep Delay, detection grace time, detection interval, detection count
   ::testing::Values(
-    std::make_tuple(EFM_FAILOVER_TEST_ID, 5000,  30000, 5000, 3),
-    std::make_tuple(EFM_FAILOVER_TEST_ID, 10000, 30000, 5000, 3),
-    std::make_tuple(EFM_FAILOVER_TEST_ID, 15000, 30000, 5000, 3),
-    std::make_tuple(EFM_FAILOVER_TEST_ID, 20000, 30000, 5000, 3),
-    std::make_tuple(EFM_FAILOVER_TEST_ID, 25000, 30000, 5000, 3),
-    std::make_tuple(EFM_FAILOVER_TEST_ID, 30000, 30000, 5000, 3),
-    std::make_tuple(EFM_FAILOVER_TEST_ID, 35000, 30000, 5000, 3),
-    std::make_tuple(EFM_FAILOVER_TEST_ID, 40000, 30000, 5000, 3),
-    std::make_tuple(EFM_FAILOVER_TEST_ID, 45000, 30000, 5000, 3),
-    std::make_tuple(EFM_FAILOVER_TEST_ID, 50000, 30000, 5000, 3)
+    std::make_tuple(EFM_FAILOVER_TEST_ID, 5000,  30000, 5000, 3)//,
+    // std::make_tuple(EFM_FAILOVER_TEST_ID, 10000, 30000, 5000, 3),
+    // std::make_tuple(EFM_FAILOVER_TEST_ID, 15000, 30000, 5000, 3),
+    // std::make_tuple(EFM_FAILOVER_TEST_ID, 20000, 30000, 5000, 3),
+    // std::make_tuple(EFM_FAILOVER_TEST_ID, 25000, 30000, 5000, 3),
+    // std::make_tuple(EFM_FAILOVER_TEST_ID, 30000, 30000, 5000, 3),
+    // std::make_tuple(EFM_FAILOVER_TEST_ID, 35000, 30000, 5000, 3),
+    // std::make_tuple(EFM_FAILOVER_TEST_ID, 40000, 30000, 5000, 3),
+    // std::make_tuple(EFM_FAILOVER_TEST_ID, 45000, 30000, 5000, 3),
+    // std::make_tuple(EFM_FAILOVER_TEST_ID, 50000, 30000, 5000, 3)
   )
 );
 
-INSTANTIATE_TEST_CASE_P(
-  EFMDetectionTimeoutTest,
-  FailoverPerformanceTest,
-  // Test Type, Sleep Delay, detection grace time, detection interval, detection count
-  ::testing::Values(
-    std::make_tuple(EFM_DETECTION_TEST_ID, 5000,  30000, 5000, 3),
-    std::make_tuple(EFM_DETECTION_TEST_ID, 10000, 30000, 5000, 3),
-    std::make_tuple(EFM_DETECTION_TEST_ID, 15000, 30000, 5000, 3),
-    std::make_tuple(EFM_DETECTION_TEST_ID, 20000, 30000, 5000, 3),
-    std::make_tuple(EFM_DETECTION_TEST_ID, 25000, 30000, 5000, 3),
-    std::make_tuple(EFM_DETECTION_TEST_ID, 30000, 30000, 5000, 3),
-    std::make_tuple(EFM_DETECTION_TEST_ID, 35000, 30000, 5000, 3),
-    std::make_tuple(EFM_DETECTION_TEST_ID, 40000, 30000, 5000, 3),
-    std::make_tuple(EFM_DETECTION_TEST_ID, 45000, 30000, 5000, 3),
-    std::make_tuple(EFM_DETECTION_TEST_ID, 50000, 30000, 5000, 3)
-  )
-);
+// INSTANTIATE_TEST_CASE_P(
+//   EFMDetectionTimeoutTest,
+//   FailoverPerformanceTest,
+//   // Test Type, Sleep Delay, detection grace time, detection interval, detection count
+//   ::testing::Values(
+//     std::make_tuple(EFM_DETECTION_TEST_ID, 5000,  30000, 5000, 3),
+//     std::make_tuple(EFM_DETECTION_TEST_ID, 10000, 30000, 5000, 3),
+//     std::make_tuple(EFM_DETECTION_TEST_ID, 15000, 30000, 5000, 3),
+//     std::make_tuple(EFM_DETECTION_TEST_ID, 20000, 30000, 5000, 3),
+//     std::make_tuple(EFM_DETECTION_TEST_ID, 25000, 30000, 5000, 3),
+//     std::make_tuple(EFM_DETECTION_TEST_ID, 30000, 30000, 5000, 3),
+//     std::make_tuple(EFM_DETECTION_TEST_ID, 35000, 30000, 5000, 3),
+//     std::make_tuple(EFM_DETECTION_TEST_ID, 40000, 30000, 5000, 3),
+//     std::make_tuple(EFM_DETECTION_TEST_ID, 45000, 30000, 5000, 3),
+//     std::make_tuple(EFM_DETECTION_TEST_ID, 50000, 30000, 5000, 3)
+//   )
+// );
